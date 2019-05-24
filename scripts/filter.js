b0ca74bab0e1822d395d4515d12748a2dad62426
PyBest9 <49530527+PyBest9@users.noreply.github.com>
2019-05-24 23:47:01 +0200

    Add files via upload

---
+++ scripts/filter.js
@@ -0,0 +1,14 @@
+function filt(){
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+}
+
+
+
+ 
